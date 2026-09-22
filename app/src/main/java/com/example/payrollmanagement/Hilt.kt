d70a5226724d4163460200ffce09e02c08138667
tamiloo7s <tamilooo757@gmail.com>
2026-09-22 17:38:02 +0530

    test msg2

--- app/src/main/java/com/example/payrollmanagement/Hilt.kt
+++ app/src/main/java/com/example/payrollmanagement/Hilt.kt
@@ -13,7 +13,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
-//added
+//added1
 
 @Module
 @InstallIn(SingletonComponent::class)
